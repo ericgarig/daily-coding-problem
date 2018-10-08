b16e489f4c1ab4b7d156848e11badbbdaa33eabd
Erik Shagdar <erik.shagdar@gmail.com>
2018-10-08 13:16:14 -0400

    initial commit

---
+++ sum-two-items-in-list.py
@@ -0,0 +1,41 @@
+"""
+Daily Coding Problem - 2018-10-08.
+
+Given a list of numbers and a number k,
+return whether any two numbers from the list add up to k.
+
+E.g.: given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17
+"""
+
+
+def sum_pick_two(the_list, k):
+    """
+    Does multiple passes to determine if sum of any 2 numbers are add to k.
+    O(n**2)
+    """
+    for i in the_list[:-1]:
+        for j in the_list[1:]:
+            if i + j == k:
+                return True
+    return False
+
+
+def sum_pick_two_single(the_list, k):
+    """
+    Does a single pass to determine if 2 numbers in a list add to k.
+    O(n)
+    """
+    while len(the_list) > 0:
+        i = the_list.pop()
+        if (k - i) in the_list:
+            return True
+    return False
+
+
+# sum_pick_two([10, 15, 3, 7], 17)
+# sum_pick_two([10, 15, 3, 7], 20)
+# sum_pick_two([10, 15, 3, 7], 25)
+
+# sum_pick_two_single([10, 15, 3, 7], 17)
+# sum_pick_two_single([10, 15, 3, 7], 20)
+# sum_pick_two_single([10, 15, 3, 7], 25)
