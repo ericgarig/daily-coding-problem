d50b515d2ae059745200b90ad2deac2ff3a0f1d4
Erik Shagdar <erik.shagdar@gmail.com>
2018-10-17 16:11:32 -0400

    added delayed function call.

---
+++ delayed-function-call.py
@@ -0,0 +1,21 @@
+"""
+Daily Coding Problem - 2018-10-17.
+
+Implement a job scheduler which takes in a function f and an integer n,
+and calls f after n milliseconds.
+"""
+import time
+
+
+def say_hi():
+    """A sample function that greets you."""
+    return 'Hello'
+
+
+def delayed_call(f, n):
+    """Call the specified function after a delay."""
+    time.sleep(n)
+    return eval(f + '()')
+
+
+# delayed_call('say_hi', 3)
