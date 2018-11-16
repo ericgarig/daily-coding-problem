a857522b9b77dd9c9fe30fa4f1ce2ec6a310621f
Erik Shagdar <erik.shagdar@gmail.com>
2018-11-16 17:56:44 -0500

    adding missing problems. swapped order of TODO and problem number in the file name.

---
+++ 036-TODO-largest-node-in-tree.py
@@ -0,0 +1,14 @@
+"""
+Daily Coding Problem - 2018-11-12.
+
+Given the root to a binary search tree, find the second largest node in
+the tree.
+"""
+
+
+def largest_node(arr):
+    """Find the next largest node."""
+    pass
+
+
+print(largest_node())
